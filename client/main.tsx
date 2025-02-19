98753e80be4dd9b39394b60dedff4aeabf9b68c5
Anderson Neves <andersonneves.dev@gmail.com>
2025-02-19 14:04:27 -0300

    feat: add react router dom

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Meteor } from "meteor/meteor";
+import { App } from "/imports/ui/App";
+import { BrowserRouter } from "react-router-dom";
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
   const root = createRoot(container!);
-  root.render(<App />);
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 });
--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Hello } from './Hello';
-import { Info } from './Info';
+import React from "react";
+import { Route, Routes } from "react-router";
+import { Login } from "./pages/login";
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   </div>
 );
