9faaf23b9405d5b65216f830ceebb57714cc15c9
Anderson Neves <andersonneves.dev@gmail.com>
2025-03-11 15:35:49 -0300

    feat: add page edit user

---
+++ client/interfaces/meteorUser.ts
@@ -0,0 +1,15 @@
+export interface UserEdit {
+  nome?: string;
+  email?: string;
+  aniversario?: string;
+  empresa?: string;
+  sexo?: string;
+  profileImage?: string;
+}
+
+export interface MeteorUser {
+  _id: string;
+  username?: string;
+  emails?: { address: string; verified: boolean }[];
+  profile?: UserEdit;
+}
